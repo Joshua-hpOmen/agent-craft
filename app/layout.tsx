2ed77794766a449065eaf3a24084ffd86899d16c
Joshua-hpOmen <146727184+JoshuaCHelsea@users.noreply.github.com>
2025-09-25 12:11:35 -0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider>
-        <ThemeProvider>
+        <ThemeProvider defaultTheme="light">
           <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
